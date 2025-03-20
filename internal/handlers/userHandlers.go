732b9771abd9bead0506ebe340f7d00644bb81e8
amarantec <marcoscarvalhop@outlook.com>
2025-03-20 09:40:33 -0300

    if response is empty return unauthorized

--- internal/handlers/userHandlers.go
+++ internal/handlers/userHandlers.go
@@ -1,35 +1,3 @@
-package handlers
-
-import (
-    "net/http"
-    "encoding/json"
-    "context"
-    "time"
-    "github.com/amarantec/move-easy/internal"
-    "github.com/amarantec/move-easy/internal/user"
-)
-
-type UserHandler struct {
-    service user.IUserService
-}
-
-func NewUserHandler(service user.IUserService) *UserHandler {
-    return &UserHandler{service: service}
-}
-
-func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
-    ctxTimeout, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-    defer cancel()
-
-    var user internal.UserRegister
-
-    if err :=
-        json.NewDecoder(r.Body).Decode(&user); err != nil {
-            http.Error(w,
-                "could not decode this request, error: " + err.Error(),
-                http.StatusBadRequest)
-        return
-    }
 
     response, err := h.service.Register(ctxTimeout, user)
     if err != nil {
@@ -65,6 +33,13 @@ func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
         http.Error(w,
             "could not validate this credentials, error: " + err.Error(),
             http.StatusInternalServerError)
+        return
+    }
+
+    if response == internal.EMPTY {
+        http.Error(w,
+            "unauthorized",
+            http.StatusUnauthorized)
         return
     }
 
