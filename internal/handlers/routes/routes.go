26b23233af30e07f39f12d587dc2617880263a23
amarantec <marcoscarvalhop@outlook.com>
2025-03-23 13:39:06 -0300

    add shared vehicle routes to mux server

--- internal/handlers/routes/routes.go
+++ internal/handlers/routes/routes.go
@@ -1,46 +1,57 @@
 package routes
 
-import(
-    "github.com/jackc/pgx/v5/pgxpool"
-    "github.com/amarantec/move-easy/internal/address"
-    "github.com/amarantec/move-easy/internal/user"
-    "github.com/amarantec/move-easy/internal/handlers"
-    "github.com/amarantec/move-easy/internal/contact"
-    "net/http"
+import (
+	"net/http"
+
+	"github.com/amarantec/move-easy/internal/address"
+	"github.com/amarantec/move-easy/internal/contact"
+	"github.com/amarantec/move-easy/internal/handlers"
+	"github.com/amarantec/move-easy/internal/sharedVehicle"
+	"github.com/amarantec/move-easy/internal/user"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func SetRoutes(conn *pgxpool.Pool) *http.ServeMux {
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-    /*
-        Address Dependency Injection
-    */
-    addrRepository := address.NewAddressRepository(conn)
-    addrService := address.NewAddressService(addrRepository)
-    addrHandler := handlers.NewAddressHandler(addrService)
+	/*
+	   Address Dependency Injection
+	*/
+	addrRepository := address.NewAddressRepository(conn)
+	addrService := address.NewAddressService(addrRepository)
+	addrHandler := handlers.NewAddressHandler(addrService)
 
-    /*
-        User Dependency Injection
-    */
+	/*
+	   User Dependency Injection
+	*/
 
-    userRepository := user.NewUserRepository(conn)
-    userService := user.NewUserService(userRepository)
-    userHandler := handlers.NewUserHandler(userService)
+	userRepository := user.NewUserRepository(conn)
+	userService := user.NewUserService(userRepository)
+	userHandler := handlers.NewUserHandler(userService)
 
-    /*
-        Contact Dependency Injection
-    */
+	/*
+	   Contact Dependency Injection
+	*/
 
-    contactRepository := contact.NewContactRepository(conn)
-    contactService := contact.NewContactService(contactRepository)
-    contactHandler := handlers.NewContactHandler(contactService)
+	contactRepository := contact.NewContactRepository(conn)
+	contactService := contact.NewContactService(contactRepository)
+	contactHandler := handlers.NewContactHandler(contactService)
 
-    /*
-        Routes
-    */
+	/*
+		Shared Vehicle Dependency Injection
+	*/
 
-    mux.Handle("/user/", http.StripPrefix("/user", userRoutes(userHandler)))
-    mux.Handle("/address/", http.StripPrefix("/address", addressRoutes(addrHandler)))
-    mux.Handle("/contact/", http.StripPrefix("/contact", contactRoutes(contactHandler)))
-    return mux
+	sharedVehicleRepository := sharedVehicle.NewSharedVehicleRepository(conn)
+	sharedVehicleService := sharedVehicle.NewSharedVehicleService(sharedVehicleRepository)
+	sharedVehicleHandler := handlers.NewSharedVehicleHandler(sharedVehicleService)
+
+	/*
+	   Routes
+	*/
+
+	mux.Handle("/user/", http.StripPrefix("/user", userRoutes(userHandler)))
+	mux.Handle("/address/", http.StripPrefix("/address", addressRoutes(addrHandler)))
+	mux.Handle("/contact/", http.StripPrefix("/contact", contactRoutes(contactHandler)))
+	mux.Handle("/shared-vehicle/", http.StripPrefix("/shared-vehicle", sharedVehicleRoutes(sharedVehicleHandler)))
+	return mux
 }
