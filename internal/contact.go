1864ab51946391f39209a94a73ea3e977f45292d
amarantec <marcoscarvalhop@outlook.com>
2025-03-24 08:28:19 -0300

    fix identation

--- internal/contact.go
+++ internal/contact.go
@@ -3,13 +3,13 @@ package internal
 import "time"
 
 type Contact struct {
-	ID			int64
-	UserID		int64
-	Name		string
-	DDI			string
-	DDD			string
-	PhoneNumber	string
-    CreatedAt   time.Time
-    Updated     *time.Time
-    DeletedAt   *time.Time
+	ID          int64
+	UserID      int64
+	Name        string
+	DDI         string
+	DDD         string
+	PhoneNumber string
+	CreatedAt   time.Time
+	UpdatedAt   *time.Time
+	DeletedAt   *time.Time
 }
