6442d2a956ce768c0f7444378b6074af9f897ed8
amarantec <marcoscarvalhop@outlook.com>
2025-03-24 08:36:01 -0300

    fix identation

--- pkg/logger/databaseLogger.go
+++ pkg/logger/databaseLogger.go
@@ -1,13 +1,14 @@
 package logger
 
 import (
-    "log"
-    "github.com/jackc/pgx/v5/tracelog"
-    "context"
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/tracelog"
 )
 
 type PgxLogger struct{}
 
 func (l *PgxLogger) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]any) {
-    log.Printf("[DATABASE] [%s] %s - %v", level, msg, data)
+	log.Printf("[QUERY] [%s] %s - %v", level, msg, data)
 }
