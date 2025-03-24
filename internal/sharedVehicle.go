f813f69e899d90bb0647eef7e955b805224c00c8
amarantec <marcoscarvalhop@outlook.com>
2025-03-24 08:30:34 -0300

    add timestamps for softdelete

--- internal/sharedVehicle.go
+++ internal/sharedVehicle.go
@@ -3,10 +3,13 @@ package internal
 import "time"
 
 type SharedVehicle struct {
-	ID			int64
-	UserID		int64
-	Latitude	float64
-	Longitude	float64
-	VehicleType	VehicleType
-	ReportedAt	time.Time
+	ID          int64
+	UserID      int64
+	Latitude    float64
+	Longitude   float64
+	VehicleType VehicleType
+	ReportedAt  time.Time
+	CreatedAt   time.Time
+	UpdatedAt   *time.Time
+	DeletedAt   *time.Time
 }
