bca570e72a20b78e5e79f32997bcfc783836b3cf
amarantec <marcoscarvalhop@outlook.com>
2025-04-14 22:46:00 -0300

    add db timestamps

--- internal/busStop.go
+++ internal/busStop.go
@@ -1,8 +1,13 @@
 package internal
 
+import "time"
+
 type BusStop struct {
-	ID			int64
-	Name		string
-	Latitude 	float64
-	Longitude 	float64
+	ID        int64
+	Name      string
+	Latitude  float64
+	Longitude float64
+	CreatedAt time.Time
+	UpdatedAt *time.Time
+	DeletedAt *time.Time
 }
