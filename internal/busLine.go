50dcc7c0f1b3b6f52786bca91cc4572aaeea4646
amarantec <marcoscarvalhop@outlook.com>
2025-04-14 22:45:52 -0300

    add db timestamps

--- internal/busLine.go
+++ internal/busLine.go
@@ -1,7 +1,14 @@
 package internal
 
+import "time"
+
 type BusLine struct {
-	ID			int64
-	Name		string
-	Schedules 	[]BusSchedules
+	ID        int64
+	Name      string
+	BusInit   BusStop
+	BusEnd    BusStop
+	Schedules []BusSchedules
+	CreatedAt time.Time
+	UpdatedAt *time.Time
+	DeletedAt *time.Time
 }
