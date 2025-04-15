86ee65fa6ac2dd099c504fd00d7cba0dacc12479
amarantec <marcoscarvalhop@outlook.com>
2025-04-14 22:59:33 -0300

    fix identation

--- internal/handlers/routes/contactRoutes.go
+++ internal/handlers/routes/contactRoutes.go
@@ -1,19 +1,20 @@
 package routes
 
 import (
-    "github.com/amarantec/move-easy/internal/handlers"
-    "github.com/amarantec/move-easy/internal/middleware"
-    "net/http"
+	"net/http"
+
+	"github.com/amarantec/move-easy/internal/handlers"
+	"github.com/amarantec/move-easy/internal/middleware"
 )
 
 func contactRoutes(handler *handlers.ContactHandler) *http.ServeMux {
-    contactMux := http.NewServeMux()
+	contactMux := http.NewServeMux()
 
-    contactMux.HandleFunc("/save-contact", middleware.Authenticate(handler.SaveContact))
-    contactMux.HandleFunc("/get-contact/{contactID}", middleware.Authenticate(handler.GetContact))
-    contactMux.HandleFunc("/list-contacts", middleware.Authenticate(handler.ListContacts))
-    contactMux.HandleFunc("/update-contact", middleware.Authenticate(handler.UpdateContact))
-    contactMux.HandleFunc("/delete-contact/{contactID}", middleware.Authenticate(handler.DeleteContact))
+	contactMux.HandleFunc("/save-contact", middleware.Authenticate(handler.SaveContact))
+	contactMux.HandleFunc("/get-contact/{contactID}", middleware.Authenticate(handler.GetContact))
+	contactMux.HandleFunc("/list-contacts", middleware.Authenticate(handler.ListContacts))
+	contactMux.HandleFunc("/update-contact", middleware.Authenticate(handler.UpdateContact))
+	contactMux.HandleFunc("/delete-contact/{contactID}", middleware.Authenticate(handler.DeleteContact))
 
-    return contactMux
+	return contactMux
 }
