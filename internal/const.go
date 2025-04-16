fd9ecaf741b5b083c6c395db2705385e45a00d5a
amarantec <marcoscarvalhop@outlook.com>
2025-04-16 20:57:02 -0300

    new const values to avoid magic number and hard coded strings

--- internal/const.go
+++ internal/const.go
@@ -3,4 +3,10 @@ package internal
 const (
 	ZERO = 0
 	EMPTY = ""
+    TEMPLATES = "templates"
+    DEFAULT_TEMPLATES_PATH = "/app/www/templates"
+    WWW = "www"
+    DEFAULT_WWW_PATH = "/app/www"
+    HTML = "*.html"
+    ENV = ".env"
 )
