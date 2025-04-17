40e50e0137957fc751a0411da6338e9472257183
amarantec <marcoscarvalhop@outlook.com>
2025-04-16 21:01:59 -0300

    clean main file, move loadenv funcs to utils/

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,118 +1,58 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "io"
-    "path/filepath"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
     "time"
 
-    "github.com/joho/godotenv"
-    "github.com/amarantec/move-easy/internal/db"
-    "github.com/amarantec/move-easy/internal"
-    "github.com/amarantec/move-easy/internal/middleware"
-    "github.com/amarantec/move-easy/internal/handlers/routes"
+	"github.com/amarantec/move-easy/internal/utils"
+	"github.com/amarantec/move-easy/internal/db"
+	"github.com/amarantec/move-easy/internal/handlers/routes"
+	"github.com/amarantec/move-easy/internal/middleware"
 )
 
 func main() {
-    loadEnv()
-    setupLogger()
+	utils.LoadEnv()
+	setupLogger()
 
-    // Wait 10 seconds for podman:postgres start
-    time.Sleep(10 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+    defer cancel()
 
-    ctx := context.Background()
-    serverPort := ":8080"
+	connectionString, err := utils.BuildConnectionString()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    connectionString, err := buildConnectionString()
-    if err != nil {
-        log.Fatal(err)
-    }
+	Conn, err := db.OpenConnection(ctx, connectionString)
+	if err != nil {
+		panic(err)
+	}
 
-    Conn, err := db.OpenConnection(ctx, connectionString)
-    if err != nil {
-        panic(err)
-    }
+	defer Conn.Close()
 
-    defer Conn.Close()
+	mux := routes.SetRoutes(Conn)
+	loggedMux := middleware.LoggerMiddleware(mux)
 
-    mux := routes.SetRoutes(Conn)
-    loggedMux := middleware.LoggerMiddleware(mux)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: loggedMux,
+	}
 
-    server := &http.Server {
-        Addr:       serverPort,
-        Handler:    loggedMux,
-    }
-
-    fmt.Printf("Server listen on: localhost%s\n", server.Addr)
-    log.Fatal(server.ListenAndServe())
-}
-
-func findEnv(path string) (string, error) {
-    filePath := filepath.Join(path, ".env")
-    if _, err := os.Stat(filePath); err == nil {
-        return filePath, nil
-    }
-
-    files, err := os.ReadDir(path)
-    if err != nil {
-        return internal.EMPTY, err
-    }
-    
-    for _, file := range files {
-        if file.IsDir() {
-            subDirPath := filepath.Join(path, file.Name())
-            subDirFilePath, err := findEnv(subDirPath)
-            if err == nil {
-                return subDirFilePath, nil
-            }
-        }
-    }
-
-    return internal.EMPTY, fmt.Errorf("file .env not found in %s or anywhere\n", path)
-}
-
-func loadEnv() {
-    path, err := os.Getwd()
-    if err != nil {
-        log.Fatal("error getting actual dir")
-    }
-
-    appPath := filepath.Dir(filepath.Dir(path))
-    pathFile, err := findEnv(appPath)
-    if err != nil {
-        log.Fatal(".env no found")
-    }
-    godotenv.Load(pathFile)
-}
-
-func buildConnectionString() (string, error) {
-    dbHost := os.Getenv("DB_HOST")
-    dbUser := os.Getenv("POSTGRES_USER")
-    dbPassword := os.Getenv("POSTGRES_PASSWORD")
-    dbName := os.Getenv("POSTGRES_DB")
-    dbPort := os.Getenv("DB_PORT")
-
-    if dbHost == "" || dbPort == "" || dbUser == "" || dbPassword == "" || dbName == "" {
-        return internal.EMPTY, fmt.Errorf("one or more environment variables are not set")
-    }
-
-    connectionString := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
-                        dbHost, dbPort, dbUser, dbPassword, dbName)
-
-    return connectionString, nil
+	fmt.Printf("Server listen on: http://localhost%s\n", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
 
 func setupLogger() {
-    logFile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        log.Fatal("erro ao abrir o arquivo de log: %v\n", err)
-    }
+	logFile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("erro ao abrir o arquivo de log: \n", err)
+	}
 
-    multiWriter := io.MultiWriter(os.Stdout, logFile)
-    log.SetOutput(multiWriter)
-    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 }
